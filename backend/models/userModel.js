6bdd9a6c0b9647f4fc827ad25c64f5086565a687
Taufiq Zarif <taufiqzarif90@gmail.com>
2024-04-07 02:17:43 +0800

    implement basic backend, middleware and user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const addressSchema = mongoose.Schema(
+  {
+    addressId: { type: String, required: true },
+    addressName: { type: String },
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipCode: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const userSchema = mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    role: { type: String, required: true, default: "user" },
+    defaultAddressId: { type: String, required: false },
+    addresses: [addressSchema],
+    phone: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+// Method to match password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Middleware to hash password before saving
+userSchema.pre("save", async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const port = process.env.PORT || 7000;
@@ -9,11 +10,19 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
+// Middleware: JSON body parser
+app.use(express.json());
+
+// Middleware: URL Encoded body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware: Cookie parser
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
-
+});
