5718ee0f6962b36c29fb3572daba07ed9b3bec2c
Taufiq Zarif <taufiqzarif90@gmail.com>
2024-04-11 12:20:41 +0800

    auth middleware protect & admin

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,48 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from './asyncHandler.js';
+import User from '../models/userModel.js';
+
+// Protect routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // Get token from header
+  token = req.cookies.jwt;
+
+  if (token) {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // GET user from DB
+    req.user = await User.findById(decoded.userId).select('-password');
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+// Admin routes
+const admin = asyncHandler(async (req, res, next) => {
+  let token;
+
+  token = req.cookies.jwt;
+
+  if (token) {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.userId).select('-password');
+
+    if (req.user && req.user.role === 'admin') {
+      next();
+    } else {
+      res.status(401);
+      throw new Error('Not authorized as an admin');
+    }
+  } else {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+export { protect, admin };
