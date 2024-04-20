e0c8e39f8ac5e81ed2da94925c6c0e1a392aacd5
syazwani <syazwani.akebal@razer.com>
2024-04-21 00:22:46 +0800

    initial simple login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
-  return <HomePage />;
+  return <LoginPage />;
 };
 
 export default App;
